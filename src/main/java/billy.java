d22f24f6d43aef0007f56378b0565ec98c92f3a6
mackerelsoup <lzttzllzttzl@gmail.com>
2025-08-17 14:19:33 +0800

    Added greeting

--- src/main/java/billy.java
+++ src/main/java/billy.java
@@ -1,5 +1,16 @@
 public class billy {
-    public static void main(String[] args) {
 
+    public static void divider() {
+        String divider = "_".repeat(50);
+        System.out.println(divider);
+    }
+
+    public static void main(String[] args) {
+        divider();
+        System.out.println("Hello, I'm billy");
+        System.out.println("What can I do for you?");
+        divider();
+        System.out.println("Bye. Hope to see you again soon!");
+        divider();
     }
 }

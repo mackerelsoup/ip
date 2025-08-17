2542a960e2628022d849172869186cad1577b639
mackerelsoup <lzttzllzttzl@gmail.com>
2025-08-17 16:18:41 +0800

    Added task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void setDone() {
+        this.isDone = true;
+    }
+
+    public void setUndone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void printStatus() {
+        System.out.printf("[%s], %s", getStatusIcon(), this.description);
+    }
+}

1ba1ee149004b12e6f52e6770ae04ebf01060f5c
mackerelsoup <lzttzllzttzl@gmail.com>
2025-08-17 14:38:24 +0800

    Added echo functionality

--- src/main/java/billy.java
+++ src/main/java/billy.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class billy {
 
     public static void divider() {
@@ -6,9 +8,21 @@ public class billy {
     }
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //actual code
         divider();
         System.out.println("Hello, I'm billy");
         System.out.println("What can I do for you?");
+        divider();
+
+        String line;
+        while(!(line = input.nextLine()).equals("bye")) {
+            divider();
+            System.out.println(line);
+            divider();
+        }
+
         divider();
         System.out.println("Bye. Hope to see you again soon!");
         divider();

5e2f42e35d64c042cd526be912138b31ce4e9d3d
zt <lzttzllzttzl@gmail.com>
2025-08-26 22:38:03 +0800

    Update Deadlines to support Optional Date Time

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,18 +1,27 @@
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 public class Deadlines extends Task{
     protected String deadline;
+    protected Optional<LocalDateTime> deadlineTime;
 
     public Deadlines(String description, String deadline) {
         super(description);
         this.deadline = deadline;
+        this.deadlineTime = Parser.tryParse(deadline, true);
     }
 
-    public Deadlines(String description, boolean done, String deadline) {
-        super(description, done);
-        this.deadline = deadline;
-    }
 
     @Override
     public void printStatus() {
-        System.out.printf("[D][%s] %s (by: %s)\n", getStatusIcon(), this.description, this.deadline);
+        System.out.printf("[D][%s] %s (by: %s)\n", getStatusIcon(), this.description,
+                this.deadlineTime.map(Parser::getTime).orElseGet(() -> this.deadline));
     }
+
+    @Override
+    public String getFileString() {
+        return String.format("D | %s | %s", this.description,
+                this.deadlineTime.map(Parser::getTime).orElseGet(() -> this.deadline));
+    }
+
 }

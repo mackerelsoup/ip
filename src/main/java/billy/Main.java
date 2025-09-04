0c7ad011c94b0665675147f118e4e8b2a36d3347
zt <lzttzllzttzl@gmail.com>
2025-09-04 15:25:46 +0800

    Add auto close functionallity

--- src/main/java/billy/Main.java
+++ src/main/java/billy/Main.java
@@ -2,8 +2,6 @@ package billy;
 
 import java.io.IOException;
 
-import billy.BillyGui;
-import billy.MainWindow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;

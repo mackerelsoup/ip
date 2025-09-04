13aa823578e0c6b5918ce529af7679d9da2f0698
zt <lzttzllzttzl@gmail.com>
2025-09-04 15:20:11 +0800

    Update Main to support GUI functionality

--- src/main/java/billy/Main.java
+++ src/main/java/billy/Main.java
@@ -1,18 +1,33 @@
 package billy;
 
+import java.io.IOException;
+
+import billy.BillyGui;
+import billy.MainWindow;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+/**
+ * A GUI for Duke using FXML.
+ */
 public class Main extends Application {
+
+    private BillyGui billy = new BillyGui();
 
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-
-        stage.setScene(scene); // Setting the stage to show our scene
-        stage.show(); // Render the stage.
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setDuke(billy);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

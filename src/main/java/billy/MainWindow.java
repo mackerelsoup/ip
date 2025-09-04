8ba71faddbc3f6666e65e46d498c5a2b7bef0ec1
zt <lzttzllzttzl@gmail.com>
2025-09-04 15:10:49 +0800

    Update image file paths

--- src/main/java/billy/MainWindow.java
+++ src/main/java/billy/MainWindow.java
@@ -22,8 +22,8 @@ public class MainWindow extends AnchorPane {
 
     private BillyGui billy;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/obmana.jpg"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/clanker.jpeg"));
 
     @FXML
     public void initialize() {

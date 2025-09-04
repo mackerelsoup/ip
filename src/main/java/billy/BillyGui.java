e2d32e1a3f8ec29bcae54f8599501279e3da77a8
zt <lzttzllzttzl@gmail.com>
2025-09-04 14:24:58 +0800

    Add BillyGui class

--- src/main/java/billy/BillyGui.java
+++ src/main/java/billy/BillyGui.java
@@ -3,6 +3,7 @@ package billy;
 import java.util.ArrayList;
 
 import billy.command.Command;
+import billy.parser.ParseResult;
 import billy.parser.Parser;
 import billy.storage.Storage;
 import billy.task.Task;
@@ -28,9 +29,9 @@ public class BillyGui {
     public BillyGui() {
         this.ui = new Ui();
         this.storage = new Storage("./data/initialList.txt");
-        ArrayList<Task> initialList = new ArrayList<>();
-        initialList = Parser.parseStorageLines(storage.readFile(), ui);
-        TaskList taskList = new TaskList(initialList);
+        ParseResult parseResult;
+        parseResult = Parser.parseStorageLines(storage.readFile(), ui);
+        taskList = new TaskList(parseResult.getTaskList());
     }
 
     /**
@@ -43,51 +44,12 @@ public class BillyGui {
         Command command = Parser.parseCommand(input);
 
         // Capture console output into a string
-        String response = ui.captureOutput(() -> command.execute(taskList, ui));
-
+        String response = command.execute(taskList, ui);
         // Save tasks if this was an exit command
         if (command.shouldExit()) {
             storage.writeFile(taskList);
         }
 
         return response;
-    }
-    /**
-     * Runs the main application loop for Billy.
-     * <p>
-     * This method:
-     * <ul>
-     *   <li>Initializes a {@link TaskList} with tasks loaded from storage.</li>
-     *   <li>Continuously prompts the user for input.</li>
-     *   <li>Parses user commands and executes them.</li>
-     *   <li>Saves tasks back to storage when the user exits.</li>
-     * </ul>
-     * </p>
-     *
-     * @param initialList the list of tasks loaded from storage
-     * @param ui the {@link Ui} instance for handling user interaction
-     * @param storage the {@link Storage} instance for reading and writing tasks
-     */
-    private void run(ArrayList<Task> initialList, Ui ui, Storage storage) {
-        TaskList taskList = new TaskList(initialList);
-        String line;
-        boolean exit = false;
-        while (!exit) {
-            line = ui.getUserInput();
-            Command command = Parser.parseCommand(line);
-            command.execute(taskList, ui);
-            exit = command.shouldExit();
-        }
-        storage.writeFile(taskList);
-        ui.printBye();
-    }
-
-    public static void main(String[] args) {
-        Ui ui = new Ui();
-        Storage storage = new Storage("./data/initialList.txt");
-        ArrayList<Task> intialList;
-
-        intialList = Parser.parseStorageLines(storage.readFile(), ui);
-        Billy.run(intialList, ui, storage);
     }
 }
